52330d581166565c13795e42870fa65db24743d9
NicholasBrown-01 <nicholasbrown.us@gmail.com>
2023-07-20 15:25:31 -0500

    Lab 08 Removed boolean because it was not needed.

--- app/src/main/java/quotes/App.java
+++ app/src/main/java/quotes/App.java
@@ -15,25 +15,23 @@ public class App {
 
     public static void getData() throws IOException {
         String path = "app/src/main/resources/recentquotes.json";
-        System.out.println(quoteRandom(path, false));
+        System.out.println(quoteRandom(path));
     }
 
-    public static String quoteRandom(String path, boolean test) throws IOException {
+    public static String quoteRandom(String path) throws IOException {
         File file = new File(path);
         FileReader reader = new FileReader(file);
         Gson gson = new Gson();
         quotes.RetrieveQuotes[] json = gson.fromJson(reader, quotes.RetrieveQuotes[].class);
 
         Random random = new Random();
-        int index;
-        if (test) {
-            index = 0;
-        } else {
-            index = random.nextInt(json.length);
-        }
-        System.out.println("Index Used: " + index);
-        String author = json[index].author + ": ";
-        String text = "\"" + json[index].text + "\"";
+
+        int quoteIndex;
+        quoteIndex = random.nextInt(json.length);
+
+        System.out.println("Index Used: " + quoteIndex);
+        String author = json[quoteIndex].author + ": ";
+        String text = "\"" + json[quoteIndex].text + "\"";
 
         return author + text;
     }
